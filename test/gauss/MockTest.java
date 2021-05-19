ff27978607aef9cb2b2c914716667f58fd8b45a4
ggrubich <g.grubich.898@studms.ug.edu.pl>
2021-05-19 16:01:29 +0200

    Initial commit

---
+++ test/gauss/MockTest.java
@@ -0,0 +1,11 @@
+package gauss;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MockTest {
+    @Test
+    public void testTrue() {
+        assertTrue(12 == 10 + 1);
+    }
+}
